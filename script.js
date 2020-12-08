76d04c1acc550264dc2ca7e493d1b987b54e7e47
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-12-08 12:20:47 -0600

    modal windows

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnOpen = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnOpen.length; i++) {
+  btnOpen[i].addEventListener('click', openModal);
+
+  btnClose.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+}
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
